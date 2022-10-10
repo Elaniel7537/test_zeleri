47b4d25e834d0eb6c4cd195e2732964f9a7fea74
Daniel <elaniel7537@gmail.com>
2022-10-10 12:17:28 -0400

    feat: Ajustes en el componente weather, integracion search

--- src/components/searchBox/index.tsx
+++ src/components/searchBox/index.tsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { useDispatch } from "react-redux";
+import { setWeatherCity } from "@redux/slices/weatherSlice";
+
+import { getWeather } from "@services/services";
 
 const SearchBox: React.FC = () => {
+  const dispatch = useDispatch();
+  const [text, setText] = useState("");
+
+  const search = async (e: any) => {
+    if (e.key === "Enter") {
+      const weatherData = await getWeather({ inputSearch: text });
+      dispatch(setWeatherCity(weatherData));
+      setText("");
+    }
+  };
+
   return (
     <div className="container mx-auto mt-10">
       <div className="mb-4">
         <input
           className="font-Montserrat font-lightshadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="username"
+          id="search"
           type="text"
-          placeholder="Buscardor de Ciudad.."
+          value={text}
+          placeholder="Introducir ciudad"
+          onChange={(e) => setText(e.target.value)}
+          onKeyPress={search}
         />
       </div>
     </div>
